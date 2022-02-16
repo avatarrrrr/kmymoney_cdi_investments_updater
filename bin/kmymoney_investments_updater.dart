688c78313715a301101fdf96211718a92e2639ae
avatarrrrr <marcosrios@protonmail.com>
2022-02-16 20:51:42 -0300

    [feat] First Commit

---
+++ bin/kmymoney_investments_updater.dart
@@ -0,0 +1,6 @@
+// Hello! This script will help you to update your investments on Kmymoney (https://kmymoney.org/) acconding of CDI (Certificado de Depósito Interbancário)
+// This script only work on kmymoney files saved in XML (Extensible Markup Language).
+
+void main(List<String> arguments) {
+  print('Hello world!');
+}
